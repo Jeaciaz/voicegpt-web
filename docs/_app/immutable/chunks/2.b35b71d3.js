319be96acb5c9522a70b4357e8f16ba79beeb1f3
Sergei Knyazev <kniazevs.v@yandex.ru>
2023-03-17 00:22:52 +0300

    feat: finished desktop landing prototype

--- docs/_app/immutable/chunks/2.b35b71d3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.8211c35b.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,56 @@
-<script>
-  import {base} from '$app/paths'
+<script lang="ts">
+	import { base } from '$app/paths'
 	const statements = ['Искусственный интеллект', 'Генерация изображений', 'Обработка голоса']
 
 	const media = [
 		{ iconSrc: '/icons/Telegram.svg', link: 'https://t.me/VoiceChatAI_bot', alt: 'Telegram icon' },
 	]
+
+	const bgId = Math.floor((Date.now() % 4000) / 1000) + 1
+
+	function constructBg(resolution: string, orientation: 'landscape' | 'portrait') {
+		return `url('${base}/pictures/landing-bg-${resolution}-${bgId}-${orientation}.png')`
+	}
+
+	const resolutionVariants = ['2k'] as const
+	const orientationVariants = ['landscape', 'portrait'] as const
+
+	const bgStyles = resolutionVariants
+		.flatMap(resolution => {
+			return orientationVariants.map(
+				orientation => `--bg-${resolution}-${orientation}: ${constructBg(resolution, orientation)}`
+			)
+		})
+		.join(';')
 </script>
 
-<div class="min-h-screen flex justify-center items-center flex-col">
-	<h1 class="text-3xl md:text-6xl font-medium font-[Montserrat] tracking-wider">VoiceGPT</h1>
-	<ul class="flex list-none items-center flex-col md:flex-row mt-5 text-lg md:text-xl">
-		{#each statements as statement}
-			<li class="text-center">{statement}</li>
-			<div aria-hidden class="last:hidden">&nbsp;•&nbsp;</div>
-		{/each}
-	</ul>
+<div
+	style={bgStyles}
+	class="min-h-screen flex justify-center flex-col bg-[url:var(--bg-2k-landscape)] portrait:bg-[url:var(--bg-2k-portrait)] dark:text-neutral-200 font-[Raleway]"
+>
+	<div class="bg-white/50 dark:bg-black/50 flex justify-center items-center flex-col py-6">
+		<h1 class="text-3xl md:text-6xl font-medium font-[Montserrat] tracking-wider">VoiceGPT</h1>
+		<ul class="flex list-none items-center flex-col md:flex-row mt-5 text-lg md:text-xl">
+			{#each statements as statement}
+				<li class="text-center">{statement}</li>
+				<div aria-hidden class="last:hidden">&nbsp;•&nbsp;</div>
+			{/each}
+		</ul>
 
-	<ul class="flex mt-5">
-		{#each media as { link, iconSrc, alt }}
-			<li>
-				<a href={link} class="inline-block p-2 rounded-full border">
-					<img src="{base}{iconSrc}" {alt} class="w-8" />
-				</a>
-			</li>
-		{/each}
-	</ul>
+		<ul class="flex mt-5">
+			{#each media as { link, iconSrc, alt }}
+				<li>
+					<a href={link} class="inline-block p-2 rounded-full border">
+						<img src="{base}{iconSrc}" {alt} class="w-8" />
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<div class="fixed bottom-0 w-screen text-center py-6 bg-white/50 dark:bg-black/50">
+		(c) copyright blabla
+	</div>
 </div>
 
 <style>
@@ -32,6 +58,13 @@
 		font-family: Montserrat;
 		font-style: normal;
 		font-weight: 600;
-		src: url('/fonts/Montserrat-SemiBold.ttf');
+		src: url('./fonts/Montserrat-SemiBold.ttf');
 	}
+
+  @font-face {
+    font-family: Raleway;
+    font-style: normal;
+    font-weight: normal;
+    src: url('./fonts/Raleway-Regular.ttf');
+  }
 </style>
